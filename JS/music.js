cab742d20d0f8bdd35f006da9970c205977f74b3
sagie111 <71749018+sagie111@users.noreply.github.com>
2022-01-23 15:56:15 +0200

    Update music.js

--- JS/music.js
+++ JS/music.js
@@ -8,15 +8,13 @@ var x = document.getElementById("myAudio");
 var y = document.getElementById("myAudio2");
 var z = document.getElementById("myAudio3"); 
 var m = document.getElementById("myAudio4");
-var c = document.getElementById("myAudio5"); 
-var a = document.getElementById("myAudio6");
+
 
 var list = [x,y,z,m,c,a].sample();
 
 function playAudio() { 
 
 
-    list.loop = true;
 
     list.play(); 
 
